60470e91fb768ceda44d0a0d39041ee271fd0985
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-06-06 17:17:20 +0530

    First Heroku Deployment - 2

--- socialgrants/production_settings.py
+++ socialgrants/production_settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
