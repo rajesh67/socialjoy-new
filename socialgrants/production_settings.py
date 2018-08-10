1ec8c8c78f1e04bed7515b15fff552db30aea379
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-08-10 19:43:29 +0530

    404 added attempt#3

--- socialgrants/production_settings.py
+++ socialgrants/production_settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['socialjoy.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'webapp.apps.SuitConfig',
+    # 'webapp.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
