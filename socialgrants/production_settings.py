518bd4bf53181f81bef1d44312b825a3a2fe9dc1
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-08-10 20:19:04 +0530

    404 added attempt#6

--- socialgrants/production_settings.py
+++ socialgrants/production_settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'socialgrants.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
